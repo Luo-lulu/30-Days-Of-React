a537ab307d18a8bd80c73b9155a482bff8ec72a3
Luo-lulu <j0928313441@gmail.com>
2020-10-26 10:23:19 +0800

    exercise: day1 Level 03

---
+++ solutions/day-01/level3.js
@@ -0,0 +1,12 @@
+function exercise() {
+  const year = document.getElementById("year").value;
+  const month = document.getElementById("month").value;
+  if (year && month) {
+    const d = new Date(year, month, 0); //取得2018/01月份有幾天
+    const n = d.getDate();
+    document.getElementById("todate").innerHTML = n;
+  } else {
+    alert("請輸入您要計算的年月!!");
+    document.getElementById("todate").innerHTML = "幾";
+  }
+}
