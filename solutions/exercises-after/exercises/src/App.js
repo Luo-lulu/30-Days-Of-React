fcdd9d10046fa42fca4d75a4f23dd4af79b5b594
Luo-lulu <j0928313441@gmail.com>
2020-11-24 11:21:38 +0800

    exercise

--- solutions/exercises-after/exercises/src/App.js
+++ solutions/exercises-after/exercises/src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./App.css";
-import "./Days/day-17/router";
-import Level2 from "./Days/day-17/router";
-//import "./Days/day-19/components/TodoItem";
-//import TodoList from "./Days/day-19/components/TodoList";
+//import "./Days/day-17/router";
+//import Level2 from "./Days/day-17/router";
+import "./Days/practice/MealForm";
+//import MealForm from "./Days/practice/MealForm";
+import MealList from "./Days/practice/MealList";
 
 function App() {
   return (
     <div className="App">
-      <Level2 />
-      {/* <TodoList /> */}
+      <MealList />
+      {/* <Level2 /> */}
     </div>
   );
 }
